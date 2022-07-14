c35e2deb82c1774ff7216c12e2ebc2d2c33dbc23
skelantros <ad3fful@yandex.ru>
2022-07-14 21:16:40 +0300

    smart constructors for dbquote.result

--- src/main/scala/ru/skelantros/wikisearch/DbQuote.scala
+++ src/main/scala/ru/skelantros/wikisearch/DbQuote.scala
@@ -11,6 +11,11 @@ trait DbQuote[F[_]] {
 
 object DbQuote {
   type Result[A] = Either[DbError, A]
+  object Result {
+    def apply[A](x: A): Result[A] = Right(x)
+    def mistake[A](msg: String): Result[A] = Left(Mistake(msg))
+    def thr[A](t: Throwable): Result[A] = Left(Thr(t))
+  }
   sealed trait DbError
   case class Mistake(msg: String) extends DbError
   case class Thr(t: Throwable) extends DbError
